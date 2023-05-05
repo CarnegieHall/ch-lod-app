20ed69b450a02b537af322b566970458043af32b
Gabe Mangiante <gabrielmangiante@gmail.com>
2023-05-05 12:44:01 -0700

    removing Debug=True

--- ch_lod/settings/base.py
+++ ch_lod/settings/base.py
@@ -19,7 +19,6 @@ SPARQL_ENDPOINT = env['SPARQL_ENDPOINT']
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
-DEBUG = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
