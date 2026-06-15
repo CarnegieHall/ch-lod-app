b34f0383f0cbdcd2f06c863b4aa38fc8690fad60
Mark Grossmann <mgrossmann@carnegiehall.org>
2026-06-15 16:01:39 -0400

    Use variable names to construct sparql endpoint
    WS-6067

--- ch_lod/settings/base.py
+++ ch_lod/settings/base.py
@@ -24,7 +24,7 @@ import environ
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 env = os.environ.copy()
-SPARQL_ENDPOINT = env['SPARQL_BASEURL'] + "/chlod/query"
+SPARQL_ENDPOINT = env['SPARQL_BASEURL'] + "/" + env['SPARQL_DB_NAME'] + "/query"
 
 
 # Application definition
