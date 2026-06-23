83ed5db10dfc817089f65f5d78bf7a01ae8e91f6
Mark Grossmann <mgrossmann@carnegiehall.org>
2026-06-23 10:20:36 -0400

    Fix CSRF issues due to CDN
    WS-6411

--- ch_lod/settings/base.py
+++ ch_lod/settings/base.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+CSRF_TRUSTED_ORIGINS = ['https://data.carnegiehall.org']
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 env = os.environ.copy()
