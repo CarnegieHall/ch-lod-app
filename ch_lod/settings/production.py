388c41bd09d8c82f7f1aba6f45ebfa64b0ed11a9
Mark Grossmann <mgrossmann@carnegiehall.org>
2026-07-22 17:08:30 -0400

    Update Storage configuration to modern format
    Django changed this.
    WS-6431

--- ch_lod/settings/production.py
+++ ch_lod/settings/production.py
@@ -38,7 +38,15 @@ COMPRESS_CSS_HASHING_METHOD = 'content'
 
 
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 
 try:
